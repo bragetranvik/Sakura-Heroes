8f9c0b8bccbd2a926079b13fdc5d46cee150a2a2
Trym <tvtrym.vaaland@gmail.com>
2020-02-03 16:22:52 +0100

    Unit script
    *Removed some errors when changing scene
    *Worked on the unit script

---
+++ Assets/Scripts/Combat/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+    public string unitName;
+    public int unitLevel;
+
+    public int baseAttack;
+    public int baseDefence;
+
+    public int baseHP;
+    public int currentHP;
+
+    public int baseMP;
+    public int currentMP;
+
+    [HideInInspector]
+    public int attack;
+    public int defence;
+    public int maxHP;
+    public int maxMP;
+
+    private void Start() {
+        attack = Convert.ToInt32(baseAttack + (unitLevel * (1+(baseAttack/100))));
+        defence = Convert.ToInt32(baseDefence + (unitLevel * (1 + (baseDefence / 100))));
+        maxHP = Convert.ToInt32(baseHP + (unitLevel * (1 + (baseHP / 100))));
+        maxMP = Convert.ToInt32(baseMP + (unitLevel * (1 + (baseMP / 100))));
+    }
+}
