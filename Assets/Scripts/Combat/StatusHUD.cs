c26e6505e8f7eb46527c0b2c4ccdb494dfc55f3d
Aleksander <aleksander-bakken@hotmail.com>
2020-02-04 12:53:09 +0100

    Small changes to file structure and battle scene
    ^

--- Assets/Scripts/Combat/StatusHUD.cs
+++ Assets/Scripts/Combat/StatusHUD.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattleHUD : MonoBehaviour
+public class StatusHUD : MonoBehaviour
 {
 
     public Text nameText;
