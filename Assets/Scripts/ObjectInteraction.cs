629a625101dc4a10caa17faea86821b9c84050f9
Samuel <samuelh@stud.ntnu.no>
2020-02-17 14:19:36 +0100

    - Added interactable sign object - Added pop-up message - Added school interior

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+///
+public class ObjectInteraction : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown("e") && playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+        else
+        {
+           
+        }
+    }
+
+    //OnTriggerEnter is called once the player is inside the collider area
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    //OnTriggerExit is called once the player has left the collider area
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
+
+/**
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour {
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }else{
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
+
+    */
