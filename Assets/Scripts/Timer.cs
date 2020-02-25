e7f4e940204d7b218453db560dc3ee934b9e8544
Brage Tranvik <brageat@stud.ntnu.no>
2020-02-25 14:00:13 +0100

    Fixed warnings from TestScene
    *Added script to change camera background color
    *Added timer script to delay execution of block

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Fungus;
+
+[EventHandlerInfo("Timers",
+    "Simple Timer",
+    "Executes the block after an amount of time has elapsed")]
+
+public class Timer : EventHandler
+{
+    public float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("TimerExpired", duration);
+    }
+
+    void TimerExpired()
+    {
+        ExecuteBlock();
+    }
+}
