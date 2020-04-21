0d0a8256a9c35d57b1e6269abab2a502145ae69c
Brage <47864178+bragetranvik@users.noreply.github.com>
2020-04-21 11:58:45 +0200

    Save/load game WIP
    Commit of just the scripts to avoid merge conflicts

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SavePlayer (GameObject player)
+    {
+        Debug.Log("Trying to save...");
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        Debug.Log("SAVED: coordinates " + data.getCoordinates());
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        Debug.Log("Trying to load...");
+        string path = Application.persistentDataPath + "/player.fun";
+
+        if (File.Exists(path))
+        {
+            Debug.Log("Path found: " + path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
