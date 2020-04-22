3232ec8d045dcd422bfac0e4b9d878de73039a4a
Trym Vaaland <trymv@stud.ntnu.no>
2020-04-22 15:18:46 +0200

    Better shop

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour {
     public GameObject player;
--- Assets/Scripts/PlayerShopInventory.cs
+++ Assets/Scripts/PlayerShopInventory.cs
@@ -5,26 +5,6 @@ using UnityEngine;
 public class PlayerShopInventory : MonoBehaviour
 {
     public List<GameObject> petShopList = new List<GameObject>();
-    public GameObject petInShop0, petInShop1, petInShop2, petInShop3, petInShop4, petInShop5, petInShop6;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        petShopList.Add(petInShop0);
-        petShopList.Add(petInShop1);
-        petShopList.Add(petInShop2);
-        petShopList.Add(petInShop3);
-        petShopList.Add(petInShop4);
-        petShopList.Add(petInShop5);
-        petShopList.Add(petInShop6);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    
-    }
 
     public GameObject GetPetInShopList(int index)
     {
